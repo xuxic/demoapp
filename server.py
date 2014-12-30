74eafbcee4c5edaa80a1a9d6117a2663a5efeaa5
xi-cheng <xi-cheng.xu@hp.com>
2014-12-30 15:28:37 -0800

    add preload

---
+++ server.py
@@ -0,0 +1,19 @@
+import web
+
+urls = (
+    "/","hello",
+    "/(.*)",'Redirect'
+)
+
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self):
+        return 'Hello world'
+
+class Redirect:
+    def GET(self, path):
+        web.seeother('/' + path + "/")
+
+if __name__ == "__main__":
+    app.run()
