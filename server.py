f4a435fe20278538bb63c8d38b9d6a3bf2037941
xi-cheng.xu <xi-cheng.xu@hp.com>
2015-01-12 22:26:59 +0800

    init

---
+++ server.py
@@ -0,0 +1,22 @@
+import web
+
+urls = (
+    "/","hello",
+<<<<<<< HEAD
+    "/(.*)",'Redirect'
+=======
+>>>>>>> master
+)
+
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self):
+        return 'Hello world'
+
+class Redirect:
+    def GET(self, path):
+        web.seeother('/' + path + "/")
+
+if __name__ == "__main__":
+    app.run()
